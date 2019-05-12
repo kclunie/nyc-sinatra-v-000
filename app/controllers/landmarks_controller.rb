c72a94a092c894ff376fdda31f40aa5145310be2
Kristen Clunie <kcluniesa@yahoo.com>
2019-05-12 00:56:16 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,9 +1,11 @@
 class LandmarksController < ApplicationController
   # add controller methods
   
-  
-  
-  
+    get '/landmarks' do
+    @landmarks = Landmark.all
+    @figures = Figure.all
+    erb :'landmarks/index'
+  end
   
   get '/landmarks/new' do
     erb :"landmarks/new"
