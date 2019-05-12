0840e6aff1ddf609be2d68a2cec16e8e701460a9
Kristen Clunie <kcluniesa@yahoo.com>
2019-05-12 01:29:24 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -11,7 +11,7 @@ class LandmarksController < ApplicationController
     erb :"landmarks/new"
   end
   
-    get '/landmarks/:id' do
+  get '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
     erb :"landmarks/show"
   end
